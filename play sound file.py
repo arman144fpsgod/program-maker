a2a7af1527bb0051051e7e0485dfb11e4cb92834
arman144fpsgod <91586986+arman144fpsgod@users.noreply.github.com>
2021-10-12 16:13:59 +0330

    playing music in pygame

--- play sound file.py
+++ play sound file.py
@@ -1,3 +1,8 @@
-from playsound import playsound
+import pygame
+pygame.mixer.init()  # Initialize the mixer module.
+sound1 = pygame.mixer.Sound('D:\myfiles\music\Mymusic.ogg')  # Load a sound.
+
 while True:
-    playsound("D:\myfiles\music\Mymusic.mp3")
+    inpt = input('Press enter to play the sound: ')
+    sound1.play()  # Play the sound.
+    print('Playing sound')
