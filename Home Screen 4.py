ddbadf30a5007a73444e3ce758128c2070ab6e09
Bardia-Sohrabi <88421534+Bardia-Sohrabi@users.noreply.github.com>
2021-10-20 16:52:28 +0330

    Just a bit cleaner

--- Home Screen 4.py
+++ Home Screen 4.py
@@ -2,7 +2,7 @@ from ursina import *
 app = Ursina()
 
 
-def lock_ckeck(level, button, level2, level3):
+def lock_check(level, button, level2, level3):
     if level == 0:
         button.tooltip = Tooltip("Locked")
         return None
@@ -43,64 +43,34 @@ def run(level, level2):
 
 
 def home():
-    global levels
-    global button1
-    global button2
-    global button3
-    global button4
-    global button5
-    global button6
-    global button7
-    global button8
-    global button9
-    global button10
-    global text
+    global levels, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, text
     text = Text(text='Computer Island', scale=5, color=color.gold, position=(-0.5, 0.4))
     button1 = Button(text=levels[0][0], scale=0.2, position=(-0.75, 0.15))
-    lock_ckeck(levels[0][1], button1, levels[0][0], 0)
+    lock_check(levels[0][1], button1, levels[0][0], 0)
     button2 = Button(text=levels[1][0], scale=0.2, position=(-0.5, 0.15))
-    lock_ckeck(levels[1][1], button2, levels[1][0], 1)
+    lock_check(levels[1][1], button2, levels[1][0], 1)
     button3 = Button(text=levels[2][0], scale=0.2, position=(-0.25, 0.15))
-    lock_ckeck(levels[2][1], button3, levels[2][0], 2)
+    lock_check(levels[2][1], button3, levels[2][0], 2)
     button4 = Button(text=levels[3][0], scale=0.2, position=(0, 0.15))
-    lock_ckeck(levels[3][1], button4, levels[3][0], 3)
+    lock_check(levels[3][1], button4, levels[3][0], 3)
     button5 = Button(text=levels[4][0], scale=0.2, position=(0.25, 0.15))
-    lock_ckeck(levels[4][1], button5, levels[4][0], 4)
+    lock_check(levels[4][1], button5, levels[4][0], 4)
     button6 = Button(text=levels[5][0], scale=0.2, position=(0.5, 0.15))
-    lock_ckeck(levels[5][1], button6, levels[5][0], 5)
+    lock_check(levels[5][1], button6, levels[5][0], 5)
     button7 = Button(text=levels[6][0], scale=0.2, position=(0.75, 0.15))
-    lock_ckeck(levels[6][1], button7, levels[6][0], 6)
+    lock_check(levels[6][1], button7, levels[6][0], 6)
     button8 = Button(text=levels[7][0], scale=0.2, position=(-0.75, -0.1))
-    lock_ckeck(levels[7][1], button8, levels[7][0], 7)
+    lock_check(levels[7][1], button8, levels[7][0], 7)
     button9 = Button(text=levels[8][0], scale=0.2, position=(-0.5, -0.1))
-    lock_ckeck(levels[8][1], button9, levels[8][0], 8)
+    lock_check(levels[8][1], button9, levels[8][0], 8)
     button10 = Button(text=levels[9][0], scale=0.2, position=(-0.25, -0.1))
-    lock_ckeck(levels[9][1], button10, levels[9][0], 9)
+    lock_check(levels[9][1], button10, levels[9][0], 9)
 
 
-button1 = 0
-button2 = 0
-button3 = 0
-button4 = 0
-button5 = 0
-button6 = 0
-button7 = 0
-button8 = 0
-button9 = 0
-button10 = 0
-finish = 0
-text = 0
-text2 = 0
-l1 = ["Level 1", 1]
-l2 = ["Level 2", 0]
-l3 = ["Level 3", 0]
-l4 = ["Level 4", 0]
-l5 = ["Level 5", 0]
-l6 = ["Level 6", 0]
-l7 = ["Level 7", 0]
-l8 = ["Level 8", 0]
-l9 = ["Level 9", 0]
-l10 = ["Level 10", 0]
-levels = [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10]
+window.fullscreen = True
+button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, finish, text, text2 =\
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+levels = [["Level 1", 1], ["Level 2", 0], ["Level 3", 0], ["Level 4", 0], ["Level 5", 0], ["Level 6", 0],\
+          ["Level 7", 0], ["Level 8", 0], ["Level 9", 0], ["Level 10", 0]]
 home()
 app.run()
